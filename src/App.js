dcf2131f084afe82a3410d8571b5784eea5487e2
kaidoverse <kaidoverse@gmail.com>
2024-02-19 13:41:47 +0000

    Update styles in App.css and index.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
+import { Footer, Blog, Features, Newone, Header, Possibility } from './containers/';
+import { CTA, Brand, Navbar } from './components';
+import './App.css';
 
 
 const App = () => {
     return (
-        <div>
-            who
+        <div className="App">
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <Newone />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
         </div>
     )
 }
