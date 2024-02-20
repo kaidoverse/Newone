712af679d0e32832032d08a06750ccfec078d38b
kaidoverse <kaidoverse@gmail.com>
2024-02-20 15:52:05 +0000

    updated navbar menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,17 @@
 import React, { useState } from 'react';
-import { RiMenu3Line, RiCloseLine} from 'react-icons/ri';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
-import logon from '../../assets/logon.png';
+import logon from '../../assets/logon.svg';
 
-const menu = () => (
-  <>
-  <p><a href="#home">Home</a></p>
-  <p><a href="#wgpt3">What is GPT3?</a></p>
-  <p><a href="#possibility">Open AI</a></p>
-  <p><a href="#features">Case Studies</a></p>
-  <p><a href="#blog">Library</a></p>
-  </>
-)
+
 const Navbar = () => {
-const [toggleMenu, setToggleMenu] = useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
     <div className="newone__navbar">
       <div className="newone__navbar-links">
         <div className="newone__navbar-links_logo">
-          <img src={logon} alt="logo" />
+          <img src={logon} alt="logo"/>
         </div>
         <div className="newone__navbar-links_container">
           <p><a href="#home">Home</a></p>
@@ -35,23 +27,26 @@ const [toggleMenu, setToggleMenu] = useState(false);
       </div>
       <div className="newone__navbar-menu">
         {toggleMenu
-          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)}/>
-          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)}/>
-        }
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
         {toggleMenu && (
-          <div className="newone__navbar-menu_container scale-up-center">
-            <div className="newone__navbar-menu_container-links">
-              <menu />
-              <div className="newone__navbar-menu_container-links-sign">
-                <p>Sign in</p>
-                <button type="button">Sign up</button>
-            </div>
-            </div>
+        <div className="newone__navbar-menu_container scale-up-center">
+          <div className="newone__navbar-menu_container-links">
+            <p><a href="#home">Home</a></p>
+            <p><a href="#wgpt3">What is GPT3?</a></p>
+            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#features">Case Studies</a></p>
+            <p><a href="#blog">Library</a></p>
           </div>
+          <div className="newone__navbar-menu_container-links-sign">
+            <p>Sign in</p>
+            <button type="button">Sign up</button>
+          </div>
+        </div>
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
