464b6bc28cc968c18a57c11b8b450fd1c7026529
kaidoverse <kaidoverse@gmail.com>
2024-02-29 11:15:00 +0000

    update styles and content in src/components/cta/cta.css and src/components/cta/CTA.jsx

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,15 @@ import './cta.css';
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='newone__cta'>
+      <div className='newone__cta-content'>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+        <p>Request Early Access to Get Started</p>
+      </div>
+      <div className='newone__cta-btn'>
+        <button type="button">Get Started</button>
+      </div>
+    </div>
   )
 }
 
