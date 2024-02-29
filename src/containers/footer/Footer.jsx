1b0881a8e0070112d7a84927e2922d5e8e96742c
kaidoverse <kaidoverse@gmail.com>
2024-02-29 14:08:12 +0000

    update styles and content of src/containers/footer/footer.css and src/containers/footer/Footer.jsx

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
 import './footer.css';
+import newoneLogo from "../../assets/logo.svg";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='newone__footer section__padding'>
+      <div className='newone__footer-heading'>
+        <h1 className='gradient__text'>Do you want to step in to the future before others</h1>
+
+      </div>
+      <div className='newone__footer-btn'>
+        <p>Request Early Access</p>
+      </div>
+
+      <div className='newone__footer-links'>
+        <div className='newone__footer-links_logo'>
+          <img src={newoneLogo} alt='logo'/>
+          <p>74 Guinea St, All Rights Reserved</p>
+        </div>
+        <div className='newone__footer-links_div'>
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+
+        <div className='newone__footer-links_div'>
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+
+        <div className='newone__footer-links_div'>
+          <h4>Get in touch</h4>
+          <p>74 Guinea St Accra Ghana</p>
+          <p>0532659323</p>
+          <p>info@payme.net</p>
+        </div>
+      </div>
+
+      <div className='newone__footer-copyright'>
+        <p>© 2024 GPT-3. All rights reserved.</p>
+      </div>
+    </div>
   )
 }
 
