70aa67ad6d9fefa59567bf0504a270b9c8493024
kaidoverse <kaidoverse@gmail.com>
2024-02-29 11:58:19 +0000

    update content in src/containers/blog/Blog.jsx and src/containers/blog/imports.js

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Article } from '../../components';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
 import './blog.css';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='newone__blog section__padding' id='blog'>
+      <div className='newone__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening, We are blogging about it.</h1>
+      </div>
+      <div className='newone__blog-container'>
+        <div className='newone__blog-container_groupA'>
+          <Article imageUrl={blog01} date="sep 26, 2024" title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+        </div>
+        <div className='newone__blog-container_groupB'>
+          <Article imageUrl={blog02} date="sep 26, 2024" title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+          <Article imageUrl={blog03} date="sep 26, 2024" title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+          <Article imageUrl={blog04} date="sep 26, 2024" title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+          <Article imageUrl={blog05} date="sep 26, 2024" title="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
+        </div>
+      </div>
+    </div>
   )
 }
 
